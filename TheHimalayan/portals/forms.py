ae8c95cda004f5848a52a245889479be3fb263bb
I_am_pie <113776750+Pipawan@users.noreply.github.com>
2023-10-28 15:04:29 +0545

    Update forms.py
    I have edited the SignUpForm to make stylish in html code.

--- TheHimalayan/portals/forms.py
+++ TheHimalayan/portals/forms.py
@@ -16,6 +16,15 @@ class SignUpForm(forms.ModelForm):
     class Meta:
         model=User
         fields=['username','password','email','first_name','last_name']
+        labels={'username':'Username','password':'Password','email':'Email','first_name':'First name','last_name':'Last Name'}
+
+        widgets={
+            'username':forms.TextInput(attrs={'class':'form-control','placeholder':'Username'}),
+            'password':forms.TextInput(attrs={'class':'form-control','placeholder':''}),
+            'email':forms.TextInput(attrs={'class':'form-control','placeholder':'Email'}),
+            'first_name':forms.TextInput(attrs={'class':'form-control','placeholder':'First Name'}),
+            'last_name':forms.TextInput(attrs={'class':'form-control','placeholder':'Last Name'}),
+        }
 
 class FeedBackForm(forms.Form):
     name=forms.CharField(max_length=100)
